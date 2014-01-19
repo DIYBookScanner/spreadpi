148d90888ee80a532040455e7177681d0945bc49
Johannes Baiter <johannes.baiter@gmail.com>
2014-01-19 22:02:07 +0100

    Fix login shell for spreads

--- delivery/scripts/04-users.sh
+++ delivery/scripts/04-users.sh
@@ -8,6 +8,6 @@ cp $DELIVERY/bashrc /root/.bashrc
 
 
 # Create spreads user
-useradd -s /usr/bin/zsh -m spreads
+useradd -s /bin/bash -m spreads
 echo "spreads:spreads" |chpasswd
 cp $DELIVERY/bashrc /home/spreads/.bashrc
