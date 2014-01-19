944f38d9775b75fb12443bdced836a6b78654dd8
Johannes Baiter <johannes.baiter@gmail.com>
2014-01-19 19:13:18 +0100

    Use bash as login shell

--- delivery/scripts/03-tools.sh
+++ delivery/scripts/03-tools.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Install some basic tools and libraries
-apt-get -y install console-common htop less locales nginx ntp openssh-server vim zsh
+apt-get -y install console-common htop less locales nginx ntp openssh-server vim
--- delivery/scripts/04-users.sh
+++ delivery/scripts/04-users.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+DELIVERY=/usr/src/delivery
+
 # Create root user
 echo "root:raspberry" | chpasswd
+cp $DELIVERY/bashrc /root/.bashrc
+
 
 # Create spreads user
 useradd -s /usr/bin/zsh -m spreads
 echo "spreads:spreads" |chpasswd
-
+cp $DELIVERY/bashrc /home/spreads/.bashrc
