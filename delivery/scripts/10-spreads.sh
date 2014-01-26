ca93d83ea71f1f47bd808d0ea2caf7234856b733
Johannes Baiter <johannes.baiter@gmail.com>
2014-01-26 22:06:40 +0100

    Update spreads script

--- delivery/scripts/10-spreads.sh
+++ delivery/scripts/10-spreads.sh
@@ -3,17 +3,30 @@
 DELIVERY=/usr/src/delivery
 
 # Install spreads dependencies
-apt-get -y install build-essential liblua5.1-0 libusb-dev nginx python2.7-dev python-netifaces python-pip python-pyexiv2 python-yaml unzip
+apt-get -y install build-essential libffi-dev libjpeg8-dev liblua5.1-0\
+            libudev-dev libusb-1.0-0-dev libusb-dev nginx python2.7-dev cython\
+            python-netifaces python-yaml unzip
 wget --continue https://www.assembla.com/spaces/chdkptp/documents/aDDsvQyhOr465JacwqjQYw/download/aDDsvQyhOr465JacwqjQYw -O /tmp/chdkptp.zip
 unzip -d /usr/local/lib/chdkptp /tmp/chdkptp.zip
 rm -rf /tmp/chdkptp.zip
+
+# Get newest pip version
+wget https://raw.github.com/pypa/pip/master/contrib/get-pip.py -O/tmp/get-pip.py
+python /tmp/get-pip.py
+
+# Install CFFI
+pip install cffi
 
 # Install spreads from GitHub
 git clone https://github.com/jbaiter/spreads.git /usr/src/spreads
 cd /usr/src/spreads
 git checkout webplugin
-pip install flask flask-compress zipstream waitress requests
-pip install -e .
+pip install -e "."
+pip install -e ".[chdkcamera]"
+pip install -e ".[web]"
+
+# Install cython-hidapi from GitHub
+pip install git+https://github.com/gbishop/cython-hidapi.git
 
 # Create spreads configuration directoy
 mkdir -p /home/spreads/.config/spreads
