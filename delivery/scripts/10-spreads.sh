72e9b2f2c57073ee716a629875fad8ff5f0c0edc
Pieter Verledens <github@gunirus.be>
2014-01-28 22:21:25 +0100

    revert to pip .deb

--- delivery/scripts/10-spreads.sh
+++ delivery/scripts/10-spreads.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
 # Install spreads dependencies
-apt-get -y install build-essential libffi-dev libjpeg8-dev liblua5.1-0\
-            libudev-dev libusb-1.0-0-dev libusb-dev nginx python2.7-dev cython\
-            python-netifaces python-yaml unzip
+apt-get -y install build-essential cython libffi-dev libjpeg8-dev liblua5.1-0\
+            libudev-dev libusb-1.0-0-dev libusb-dev nginx python2.7-dev\
+            python-pyexiv2 python-netifaces python-pip python-yaml unzip 
 wget --continue https://www.assembla.com/spaces/chdkptp/documents/aDDsvQyhOr465JacwqjQYw/download/aDDsvQyhOr465JacwqjQYw -O /tmp/chdkptp.zip
 unzip -d /usr/local/lib/chdkptp /tmp/chdkptp.zip
 rm -rf /tmp/chdkptp.zip
 
 # Get newest pip version
-wget https://raw.github.com/pypa/pip/master/contrib/get-pip.py -O/tmp/get-pip.py
-python /tmp/get-pip.py
+
 
 # Install CFFI
 pip install cffi
@@ -19,6 +18,7 @@ pip install cffi
 git clone https://github.com/jbaiter/spreads.git /usr/src/spreads
 cd /usr/src/spreads
 git checkout webplugin
+pip install flask flask-compress zipstream waitress requests
 pip install -e "."
 pip install -e ".[chdkcamera]"
 pip install -e ".[web]"
