eeacfa5707729ce434acbfd9b9603099cd2cddf7
Mark Van den Borre <mark+github@markvdb.be>
2014-02-23 21:21:59 +0100

    remove python-exiv2 debian dependency

--- delivery/scripts/10-spreads.sh
+++ delivery/scripts/10-spreads.sh
@@ -7,7 +7,7 @@ set -e
 # Install spreads dependencies
 apt-get -y install build-essential libffi-dev libjpeg8-dev liblua5.1-0\
             libudev-dev libusb-1.0-0-dev libusb-dev libyaml-dev nginx\
-            python2.7-dev python-pyexiv2 python-virtualenv unzip
+            python2.7-dev python-virtualenv unzip
 wget --continue https://www.assembla.com/spaces/chdkptp/documents/aDDsvQyhOr465JacwqjQYw/download/aDDsvQyhOr465JacwqjQYw -O /tmp/chdkptp.zip
 unzip -d /usr/local/lib/chdkptp /tmp/chdkptp.zip
 rm -rf /tmp/chdkptp.zip
