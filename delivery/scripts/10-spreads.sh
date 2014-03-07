b262fbb550bcb354b06009820b851962536b032f
Mark Van den Borre <mark+github@markvdb.be>
2014-03-07 14:13:51 +0200

    Update chdkptp binary permissions

--- delivery/scripts/10-spreads.sh
+++ delivery/scripts/10-spreads.sh
@@ -10,6 +10,8 @@ apt-get -y install build-essential libffi-dev libjpeg8-dev liblua5.1-0\
             python2.7-dev python-virtualenv unzip
 wget --continue https://www.assembla.com/spaces/chdkptp/documents/aDDsvQyhOr465JacwqjQYw/download/aDDsvQyhOr465JacwqjQYw -O /tmp/chdkptp.zip
 unzip -d /usr/local/lib/chdkptp /tmp/chdkptp.zip
+# User spreads should be able to use chdkptp...
+chmod -R 755 /usr/local/lib/chdkptp/
 rm -rf /tmp/chdkptp.zip
 
 # Install all things python as non-root user "spreads" in a virtualenv.
