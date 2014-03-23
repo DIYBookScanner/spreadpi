d735430fd4413c66eced87340d5d5537c0eee14e
Mark Van den Borre <mark+github@markvdb.be>
2014-03-23 01:40:02 +0100

    change firmware update from wget to curl

--- delivery/scripts/02-firmware.sh
+++ delivery/scripts/02-firmware.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Install rpi-update and dependencies
-apt-get -y install git-core binutils ca-certificates || exit 1
-wget --continue https://raw.github.com/Hexxeh/rpi-update/master/rpi-update -O /usr/bin/rpi-update || exit 1
+apt-get -y install curl git-core binutils ca-certificates || exit 1
+curl -L https://raw.github.com/Hexxeh/rpi-update/master/rpi-update --output /usr/bin/rpi-update || exit 1
 chmod +x /usr/bin/rpi-update || exit 1
 mkdir -p /lib/modules/3.1.9+ || exit 1
 touch /boot/start.elf || exit 1
