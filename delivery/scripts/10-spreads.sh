fc6be9195539b8ad103bc0085ee65fa7b3fc1c68
Mark Van den Borre <mark+github@markvdb.be>
2014-03-25 19:15:54 +0100

    add dbus and hidapi deps

--- delivery/scripts/10-spreads.sh
+++ delivery/scripts/10-spreads.sh
@@ -5,9 +5,10 @@
 set -e
 
 # Install spreads dependencies
-apt-get -y install build-essential libffi-dev libjpeg8-dev liblua5.1-0\
-            libudev-dev libusb-1.0-0-dev libusb-dev libyaml-dev nginx\
-            python2.7-dev python-virtualenv unzip
+apt-get -y install build-essential libdbus-1-dev libdbus-glib-1-dev\
+            libffi-dev libjpeg8-dev liblua5.1-0 libudev-dev\
+            libusb-1.0-0-dev libusb-dev libusbhid-common libyaml-dev\
+             nginx python2.7-dev python-virtualenv unzip
 wget --continue https://www.assembla.com/spaces/chdkptp/documents/aH1W4CQbmr46hcacwqjQYw/download/aH1W4CQbmr46hcacwqjQYw -O /tmp/chdkptp.zip
 unzip -d /usr/local/lib/chdkptp /tmp/chdkptp.zip
 # User spreads should be able to use chdkptp...
