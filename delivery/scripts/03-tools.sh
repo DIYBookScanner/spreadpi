6fb5cdc6f2d8949e4237a5670376162dc669c5e3
Mark Van den Borre <mark+github@markvdb.be>
2014-03-25 21:00:23 +0100

    disable cifs-utils
    avoid problems with winbind complaining about proc

--- delivery/scripts/03-tools.sh
+++ delivery/scripts/03-tools.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Install some basic tools and libraries
-apt-get -y install cifs-utils console-common htop less locales nginx ntp openssh-server sudo vim || exit 1
+apt-get -y install console-common htop less locales nginx ntp openssh-server sudo vim || exit 1
 
 # User spreads should be able to shut the system down (used in web interface)
 echo 'spreads spreadpi = (root) NOPASSWD: /sbin/shutdown' >> /etc/sudoers
