635ee9d12d8ee72ce632f5e8d552e2190d1f68af
Mark Van den Borre <mark+github@markvdb.be>
2014-03-25 21:33:35 +0100

    Update 03-tools.sh

--- delivery/scripts/03-tools.sh
+++ delivery/scripts/03-tools.sh
@@ -2,6 +2,7 @@
 
 # Install some basic tools and libraries
 apt-get -y install console-common htop less locales nginx ntp openssh-server sudo vim || exit 1
+apt-get install --no-install-recommends cifs-utils
 
 # User spreads should be able to shut the system down (used in web interface)
 echo 'spreads spreadpi = (root) NOPASSWD: /sbin/shutdown' >> /etc/sudoers
