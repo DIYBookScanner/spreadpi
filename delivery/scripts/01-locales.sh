501160d5ea3f632a349c2a62762b0cc1e1ef5418
Johannes Baiter <johannes.baiter@gmail.com>
2014-07-23 09:09:48 +0200

    Fix locales installation order

--- delivery/scripts/01-locales.sh
+++ delivery/scripts/01-locales.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
+apt-get install locales
+
 echo -e 'LANG="en_US.UTF-8"\nLANGUAGE="en_US:en"\n' > /etc/default/locale
 sed -i -e "s/# en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/g" /etc/locale.gen
 locale-gen en_US.UTF-8
