6ba0a67ad365f75b6ede9ce010a4c5d20743fce8
Johannes Baiter <johannes.baiter@gmail.com>
2014-07-23 02:01:40 +0200

    Fix locale generation

--- delivery/scripts/06-locales.sh
+++ delivery/scripts/06-locales.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
 
-locale-gen --purge en_US.UTF-8
 echo -e 'LANG="en_US.UTF-8"\nLANGUAGE="en_US:en"\n' > /etc/default/locale
+sed -i -e "s/# en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/g" /etc/locale.gen
+locale-gen en_US.UTF-8
