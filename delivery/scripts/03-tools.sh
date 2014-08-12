bfaa810ce6558072da45f1e3de33eeed9a32cd1a
Johannes Baiter <johannes.baiter@gmail.com>
2014-08-12 22:19:05 +0200

    Fix hostname errors

--- delivery/scripts/03-tools.sh
+++ delivery/scripts/03-tools.sh
@@ -6,3 +6,6 @@ apt-get -y install --no-install-recommends cifs-utils console-common htop less n
 
 # User spreads should be able to shut the system down (used in web interface)
 echo 'spreads spreadpi = (root) NOPASSWD: /sbin/shutdown' >> /etc/sudoers
+
+# Fix 'unable to resolve host spreadpi' errors
+sed -i -e 's/\(127.0.0.1.*\)/\1 spreadpi/g' /etc/hosts
