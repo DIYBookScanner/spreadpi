2af055beb147e44b811407aa6f00bb28d7186364
Johannes Baiter <johannes.baiter@gmail.com>
2014-08-12 22:03:47 +0200

    Redirect from port 80 instead of running on it
    This allows spreads to still run as a regular user. Instead of running
    the WSGI application on port 80 (which would require root) we add an
    iptables rule that redirects from port 80 to port 5000.

--- delivery/scripts/10-spreads.sh
+++ delivery/scripts/10-spreads.sh
@@ -24,3 +24,7 @@ chmod a+x /etc/init.d/spread
 
 # Add spreads init script to default boot sequence
 update-rc.d spread defaults
+
+# Add iptables rule to forward traffic from port 80 to spreads running on
+# port 5000
+iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 5000
