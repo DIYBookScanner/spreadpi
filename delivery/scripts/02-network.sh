b13276cba2e63fe93cafd1cad3cf68d91e8eb8f3
Johannes Baiter <johannes.baiter@gmail.com>
2014-08-16 22:27:44 +0200

    Fix iptables rules

--- delivery/scripts/02-network.sh
+++ delivery/scripts/02-network.sh
@@ -1,9 +1,21 @@
 cat <<EOM >/etc/network/if-pre-up.d/iptables
+#!/bin/sh
 /sbin/iptables-restore < /etc/iptables.up.rules
 EOM
 
 chmod +x /etc/network/if-pre-up.d/iptables
 
 cat <<EOM >/etc/iptables.up.rules
--A PREROUTING -p tcp -m tcp --dport 80 -j REDIRECT --to-ports 5000
+*nat
+:PREROUTING ACCEPT [1:60]
+:INPUT ACCEPT [1:60]
+:OUTPUT ACCEPT [3:300]
+:POSTROUTING ACCEPT [3:300]
+-A PREROUTING -i eth0 -p tcp -m tcp --dport 80 -j REDIRECT --to-ports 5000
+COMMIT
+*filter
+:INPUT ACCEPT [290:21584]
+:FORWARD ACCEPT [0:0]
+:OUTPUT ACCEPT [200:20552]
+COMMIT
 EOM
