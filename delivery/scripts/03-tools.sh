d1ef447434948d68ccb2f6e8555db51be2450cce
Gareth Edwards <gareth@edwardsfamily.org.uk>
2014-09-13 11:13:19 +0100

    Fix syntax error in sudoers entry for spreads user

--- delivery/scripts/03-tools.sh
+++ delivery/scripts/03-tools.sh
@@ -5,7 +5,7 @@ set -e
 apt-get -y install --no-install-recommends cifs-utils console-common htop less ntp openssh-server sudo vim
 
 # User spreads should be able to shut the system down (used in web interface)
-echo 'spreads = ALL=NOPASSWD: /sbin/shutdown' >> /etc/sudoers
+echo 'spreads ALL=NOPASSWD: /sbin/shutdown' >> /etc/sudoers
 
 # Fix 'unable to resolve host spreadpi' errors
 sed -i -e 's/\(127.0.0.1.*\)/\1 spreadpi/g' /etc/hosts
